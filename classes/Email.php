56a21611bf28156c1af328f0c1df184b1aa730ea
Erik1104 <steve110453@gmail.com>
2023-06-01 11:33:01 -0600

    nuevas variables de entorno y cambio en rutas

--- classes/Email.php
+++ classes/Email.php
@@ -2,6 +2,9 @@
 
 namespace Classes;
 use PHPMailer\PHPMailer\PHPMailer;
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable('../includes/.env');
+$dotenv->safeLoad();
 
 class Email {
 
@@ -17,13 +20,14 @@ class Email {
     }
     
     public function enviarConfirmacion () {
-      $mail = new PHPMailer();     
+      $mail = new PHPMailer();
       $mail->isSMTP();
-      $mail->Host = 'smtp.mailtrap.io';
+      $mail->Host = $_ENV['MAIL_HOST'];
       $mail->SMTPAuth = true;
-      $mail->Port = 2525;
-      $mail->Username = '2e2107df9c88f8';
-      $mail->Password = 'd43d47c44c4e23';
+      $mail->Username = $_ENV['MAIL_USER'];
+      $mail->Password = $_ENV['MAIL_PASSWORD'];
+      $mail->SMTPSecure = 'tls';
+      $mail->Port = $_ENV['MAIL_PORT'];
 
       $mail->setFrom('cuentasprojectflow@gmail.com');
       $mail->addAddress('cuentasprojectflow@gmail.com', 'ProjectFlow.com' );
@@ -34,7 +38,7 @@ class Email {
 
       $contenido = "<html>";
       $contenido .= "<p><strong> Hola " . $this->nombre . "</strong> Has creado tu cuenta en ProjectFlow, solo debes confirmala presionando el siguiente enlace</p>";
-      $contenido .= "<p>Presiona aquí: <a href='/confirmar?token=" . $this->token."'>Restablecer contraseña </a> </p>";
+      $contenido .= "<p>Presiona aquí: <a href='". $_ENV['SERVER_HOST'] ."/confirmar?token=" . $this->token."'>Confirmar cuenta </a> </p>";
       $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
       $contenido .= "</html>";
       $mail->Body = $contenido;
@@ -45,13 +49,14 @@ class Email {
     public function enviarInstrucciones () {
  
       //crear el objeto de email
-      $mail = new PHPMailer();     
+      $mail = new PHPMailer();
       $mail->isSMTP();
-      $mail->Host = 'smtp.mailtrap.io';
+      $mail->Host = $_ENV['MAIL_HOST'];
       $mail->SMTPAuth = true;
-      $mail->Port = 2525;
-      $mail->Username = '2e2107df9c88f8';
-      $mail->Password = 'd43d47c44c4e23';
+      $mail->Username = $_ENV['MAIL_USER'];
+      $mail->Password = $_ENV['MAIL_PASSWORD'];
+      $mail->SMTPSecure = 'tls';
+      $mail->Port = $_ENV['MAIL_PORT'];
     
       $mail->setFrom('cuentas@projectflow.com');
       $mail->addAddress('cuentas@projectflow.com', 'ProjectFlow.com');
@@ -63,7 +68,7 @@ class Email {
     
       $contenido = "<html>";
       $contenido .= "<p><strong> Hola " . $this->nombre . "</strong> Has solicitado restablecer tu contraseña, sigue el siguiente enlace para hacerlo. </p>";
-      $contenido .= "<p>Presiona aquí: <a href='/restablecer?token="  . $this->token . "'>Restablecer contraseña </a> </p>";
+      $contenido .= "<p>Presiona aquí: <a'". $_ENV['SERVER_HOST'] ."/restablecer?token=" . $this->token."'>>Restablecer contraseña </a> </p>";
       $contenido .= "<p> Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
       $contenido .= "</html>";
       $mail->Body = $contenido;
